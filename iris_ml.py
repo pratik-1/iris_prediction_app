c3ff109df64ceb4df982b5814d75cfcbf49e259a
pratik-1 <nandekarpratik04@gmail.com>
2022-02-14 00:43:57 +0000

    added visualisations

--- iris_ml.py
+++ iris_ml.py
@@ -3,8 +3,14 @@ import pickle
 import numpy as np
 import streamlit as st
 import pandas as pd
+from bokeh.layouts import gridplot
 from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+
+
+from bokeh.plotting import figure, column
+from bokeh.models import ColumnDataSource
+from bokeh.models import CategoricalColorMapper
+
 
 st.write("""
 # Iris Flower Prediction App
@@ -37,9 +43,12 @@ else:
 
 iris = datasets.load_iris()
 X = pd.DataFrame(iris.data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+y = iris['target']
 df = pd.concat([input_features, X], axis=0, ignore_index=True)
 
-# st.write(iris)
+iris1 = pd.DataFrame(X, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+iris1['species1'] = iris['target_names'][y]
+
 df=df[:1]
 
 st.subheader('User Input parameters')
@@ -59,3 +68,57 @@ st.write(prediction_target[prediction])
 
 st.subheader('Prediction Probability')
 st.write(prediction_probs)
+
+
+def draw_plots_layouts(df, input_features):
+    source = ColumnDataSource(df)
+    color_mapper = CategoricalColorMapper(factors=['setosa', 'versicolor', 'virginica'],
+                                          palette=['red', 'blue', 'green'])
+
+    p1 = figure(x_axis_label='Sepal Length', y_axis_label='Sepal Width', tools='box_select,lasso_select')
+    p1.circle('sepal_length', 'sepal_width', size=8, source=source, legend_field='species1',
+              color=dict(field='species1', transform=color_mapper))
+    p1.circle(input_features['sepal_length'], input_features['sepal_width'], size=10, color='yellow', legend_label='input feature')
+
+    p2 = figure(x_axis_label='Petal Length', y_axis_label='Petal Width', tools='box_select,lasso_select')
+    p2.circle('petal_length', 'petal_width', size=8, source=source, legend_field='species1',
+              color=dict(field='species1', transform=color_mapper))
+    p2.circle(input_features['petal_length'], input_features['petal_width'], size=10, color='yellow',
+              legend_label='input feature')
+    p2.legend.location = 'bottom_right'
+
+    p3 = figure(x_axis_label='Sepal Length', y_axis_label='Petal Length', tools='box_select,lasso_select')
+    p3.circle('sepal_length', 'petal_length', size=8, source=source, legend_field='species1',
+              color=dict(field='species1', transform=color_mapper))
+    p3.circle(input_features['sepal_length'], input_features['petal_length'], size=10, color='yellow',
+              legend_label='input feature')
+    p3.legend.location = 'bottom_right'
+
+    p4 = figure(x_axis_label='Sepal Width', y_axis_label='Petal Width', tools='box_select,lasso_select')
+    p4.circle('sepal_width', 'petal_width', size=8, source=source, legend_field='species1',
+              color=dict(field='species1', transform=color_mapper))
+    p4.circle(input_features['sepal_width'], input_features['petal_width'], size=10, color='yellow',
+              legend_label='input feature')
+    p4.legend.location = 'center_right'
+    p1.x_range = p3.x_range
+    p2.x_range = p4.x_range
+    p1.y_range = p2.y_range = p3.y_range = p4.y_range
+
+    # Row Layout
+    # layout=row(p1,p2)
+
+    # Column Layout
+    # layout = column(p1, p2, p3, p4)
+
+
+    # Grid Layout
+    row1 = [p1, p2]
+    row2 = [p3, p4]
+    layout = gridplot([row1, row2])
+
+    # show(layout)
+    st.bokeh_chart(layout, use_container_width=True)
+
+
+st.subheader('Visualisations')
+draw_plots_layouts(iris1, input_features)
